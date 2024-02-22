310d8fdaf85fb11f60299b0e643211d6d3bdc6eb
Ollyveroso <126161749+Ollyveroso@users.noreply.github.com>
2024-02-22 10:14:55 -0300

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Footer = () => {
+  return (
+    <View style={styles.footer}>
+      <Text style={styles.footerText}>Rodapé</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  footer: {
+    backgroundColor: '#3498db',
+    padding: 15,
+    alignItems: 'center',
+  },
+  footerText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+});
+
+export default Footer;
